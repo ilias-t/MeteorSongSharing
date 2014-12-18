ef9c671885abac358e3cc48944053578194b286c
iliastsangaris <iliastsangaris@gmail.com>
2014-12-18 12:48:16 -0500

    tiles index works with delete button

--- client/views/tiles/tile.js
+++ client/views/tiles/tile.js
@@ -1,21 +1,25 @@
 if(Meteor.isClient) {
   //****** GET ******//
   //Index
-  Template.body.helpers({
+  Template.tileList.helpers({
     tiles: function() {
       return Tiles.find({});
     }
   });
-
+  //Delete
+  Template.tileItem.events({
+    "click #delete-tile": function() {
+      Tiles.remove(this._id);
+    }
+  });
   //Show
   // Template.tilePage.helpers({ currentTile: function() {
   //   return Tiles.findOne(Session.get('currentTileId')); }
   // });
-
   //****** POST ******//
   //New
   Template.tileNew.events({
-    "submit .tile-new": function (e) {
+    "submit .tile-new": function(e) {
       // This function is called when the new task form is submitted
       var song = e.target.song.value;
       var message = e.target.message.value;
@@ -24,19 +28,11 @@ if(Meteor.isClient) {
         message: message,
         createdAt: new Date() // current time
       });
-
       // Clear form
       e.target.song.value = "";
       e.target.message.value = "";
-
       // Prevent default form submit
       return false;
-    }
-  });
-  //Delete
-  Template.tileNew.events({
-    "click #submit": function(e) {
-      Tiles.remove(this._id);
     }
   });
 }
