f69e1acb6eed945cda080d23be0b9cc29d250cf0
iliastsangaris <iliastsangaris@gmail.com>
2014-12-23 00:52:51 -0500

    landing page

--- client/lib/router.js
+++ client/lib/router.js
@@ -4,6 +4,21 @@
 //   waitOn: function() { return Meteor.subscribe('posts'); }
 // });
 
+//Hooks
+Router.onBeforeAction(requireLogin);
+var requireLogin = function() { if (! Meteor.user()) {
+  this.render('landingPage'); } else {
+  this.next(); }
+}
+
+//Configure
+// Router.configure({
+//   layoutTemplate: 'layout',
+//   loadingTemplate: 'loading',
+//   notFoundTemplate: 'notFound',
+//   waitOn: function() { return Meteor.subscribe('posts'); }
+// });
+
 //Root
 Router.route('/', function() {
   this.render('tileList');
